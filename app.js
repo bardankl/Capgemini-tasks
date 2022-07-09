8e166550f2fc6c27f292c54cd8baf1b88c1b4f7b
Andrzej J <75665204+Jaga7@users.noreply.github.com>
2022-07-09 13:06:45 +0200

    Refactored

--- app.js
+++ app.js
@@ -12,48 +12,65 @@ const CLASS_NAMES = {
   todoCardButton: "todo-card-button",
 }
 
-const onSubmit = (e) => {
-  e.preventDefault()
-  if (isTodoCardBeingEdited) {
-    todoCardBeingEdited.querySelector(
-      `.${CLASS_NAMES.todoCardTitle}`
-    ).innerText = e.target[0].value
-    todoCardBeingEdited.querySelector(
-      `.${CLASS_NAMES.todoCardBody}`
-    ).innerText = e.target[1].value
+const editTodo = (e) => {
+  todoCardBeingEdited.querySelector(`.${CLASS_NAMES.todoCardTitle}`).innerText =
+    e.target[0].value
+  todoCardBeingEdited.querySelector(`.${CLASS_NAMES.todoCardBody}`).innerText =
+    e.target[1].value
+  isTodoCardBeingEdited = false
+  todoCardBeingEdited = ""
+  e.target[2].innerText = "Create todo"
+}
 
-    isTodoCardBeingEdited = false
-    todoCardBeingEdited = ""
-    e.target[2].innerText = "Create todo"
-  } else {
-    todoContainer.innerHTML += `<div class=${CLASS_NAMES.todoCard}>
+const createNewTodo = (e) => {
+  todoContainer.innerHTML += `<div class=${CLASS_NAMES.todoCard}>
     <div class=${CLASS_NAMES.todoCardText}>
   <div class=${CLASS_NAMES.todoCardTitle}>${e.target[0].value}</div>
   <div class=${CLASS_NAMES.todoCardBody}>${e.target[1].value}</div></div>
   <div class=${CLASS_NAMES.todoCardButtons}><button class="${CLASS_NAMES.todoCardButton} ${CLASS_NAMES.editButton}">Edit</button><button class="${CLASS_NAMES.todoCardButton} ${CLASS_NAMES.completeButton}">Complete</button></div></div>
   `
-  }
+}
+
+const clearFormInputs = (e) => {
   e.target[0].value = ""
   e.target[1].value = ""
 }
 
+const onSubmit = (e) => {
+  e.preventDefault()
+  if (isTodoCardBeingEdited) {
+    editTodo(e)
+  } else {
+    createNewTodo(e)
+  }
+  clearFormInputs(e)
+}
+
+const putTodoValuesInForm = (e) => {
+  isTodoCardBeingEdited = true
+  todoCardBeingEdited = e.target.closest(`.${CLASS_NAMES.todoCard}`)
+  createTodoForm[0].value = e.target
+    .closest(`.${CLASS_NAMES.todoCard}`)
+    .querySelector(`.${CLASS_NAMES.todoCardTitle}`).innerText
+  createTodoForm[1].value = e.target
+    .closest(`.${CLASS_NAMES.todoCard}`)
+    .querySelector(`.${CLASS_NAMES.todoCardBody}`).innerText
+  createTodoForm[2].innerText = "Save todo"
+  createTodoForm[0].focus()
+}
+
+const completeTodo = (e) => {
+  e.target
+    .closest(`.${CLASS_NAMES.todoCard}`)
+    .classList.add(CLASS_NAMES.todoCardComplete)
+}
+
 const onClick = (e) => {
   if (e.target.classList.contains(CLASS_NAMES.editButton)) {
-    isTodoCardBeingEdited = true
-    todoCardBeingEdited = e.target.closest(`.${CLASS_NAMES.todoCard}`)
-    createTodoForm[0].value = e.target
-      .closest(`.${CLASS_NAMES.todoCard}`)
-      .querySelector(`.${CLASS_NAMES.todoCardTitle}`).innerText
-    createTodoForm[1].value = e.target
-      .closest(`.${CLASS_NAMES.todoCard}`)
-      .querySelector(`.${CLASS_NAMES.todoCardBody}`).innerText
-    createTodoForm[2].innerText = "Save todo"
-    createTodoForm[0].focus()
+    putTodoValuesInForm(e)
   }
   if (e.target.classList.contains(CLASS_NAMES.completeButton)) {
-    e.target
-      .closest(`.${CLASS_NAMES.todoCard}`)
-      .classList.add(CLASS_NAMES.todoCardComplete)
+    completeTodo(e)
   }
 }
 
